33457f3c47a8d2a003d43e33fea3d4ad75fc5d55
dcd_100005679501_15594648816 <dcd_100005679501_15594648816@git.cloud.tencent.com>
2019-07-25 15:03:31 +0800

    Circle

--- app/src/main/java/com/david/opengl/MainActivity.java
+++ app/src/main/java/com/david/opengl/MainActivity.java
@@ -4,7 +4,8 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.david.opengl.render.SimpleRenderer;
+import com.david.opengl.render.CircleRenderer;
+import com.david.opengl.render.ColorTrianglesRenderer;
 
 public class MainActivity extends AppCompatActivity {
     private GLSurfaceView mGLSurfaceView;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(mGLSurfaceView);
         //设置版本
         mGLSurfaceView.setEGLContextClientVersion(3);
-        GLSurfaceView.Renderer renderer = new SimpleRenderer();
+        GLSurfaceView.Renderer renderer = new CircleRenderer();
         mGLSurfaceView.setRenderer(renderer);
     }
 }
--- app/src/main/java/com/david/opengl/render/CircleRenderer.java
+++ app/src/main/java/com/david/opengl/render/CircleRenderer.java
@@ -2,10 +2,12 @@ package com.david.opengl.render;
 
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -18,32 +20,21 @@ import static com.david.opengl.util.RenderUtil.linkProgram;
  * @Create: 2019/6/25 17:31
  * @Description:
  */
-public class SimpleRenderer implements GLSurfaceView.Renderer {
-//    private float[] vertexPoints = new float[]{
-//            0.0f, 0.5f, 0.0f,
-//            -0.5f, -0.5f, 0.0f,
-//            0.5f, -0.5f, 0.0f
-//    };
+public class CircleRenderer implements GLSurfaceView.Renderer {
 
-    float[] vertexPoints = {
-            // 第一个点就是三角形扇的中心点
-            0f, 0f,
-            -0.5f, -0.8f,
-            0.5f, -0.8f,
-            0.5f, 0.8f,
-            -0.5f, 0.8f,
-            -0.5f, -0.8f
-            // 重复第二个点，使三角形扇闭合
-    };
     private final FloatBuffer vertexBuffer;
+    private int uMatrixLocation;
+    private final float[] mMatrix = new float[16];
+
     /**
      * 顶点着色器
      */
     private String vertextShader =
             "#version 300 es\n" +
                     "layout (location = 0) in vec4 vPosition;\n" +
+                    "uniform mat4 u_Matrix;\n" +
                     "void main() {\n" +
-                    "     gl_Position  = vPosition;\n" +
+                    "     gl_Position  = u_Matrix * vPosition;;\n" +
                     "     gl_PointSize = 10.0;\n" +
                     "}\n";
 
@@ -59,13 +50,13 @@ public class SimpleRenderer implements GLSurfaceView.Renderer {
                     "}\n";
 
 
-    public SimpleRenderer() {
+    public CircleRenderer() {
         //分配内存空间,每个浮点型占4字节空间
-        vertexBuffer = ByteBuffer.allocateDirect(vertexPoints.length * 4)
+        vertexBuffer = ByteBuffer.allocateDirect(createPositions().length * 4)
                 .order(ByteOrder.nativeOrder())
                 .asFloatBuffer();
         //传入指定的坐标数据
-        vertexBuffer.put(vertexPoints);
+        vertexBuffer.put(createPositions());
         vertexBuffer.position(0);
     }
 
@@ -77,33 +68,65 @@ public class SimpleRenderer implements GLSurfaceView.Renderer {
         final int vertexShaderId = compileShader(GLES30.GL_VERTEX_SHADER, vertextShader);
         final int fragmentShaderId = compileShader(GLES30.GL_FRAGMENT_SHADER, fragmentShader);
         //在OpenGLES环境中使用程序片段
-        GLES30.glUseProgram(linkProgram(vertexShaderId, fragmentShaderId));
+        int mProgram = linkProgram(vertexShaderId, fragmentShaderId);
+        GLES30.glUseProgram(mProgram);
+
+        uMatrixLocation = GLES30.glGetUniformLocation(mProgram, "u_Matrix");
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         GLES30.glViewport(0, 0, width, height);
+        float aspectRatio = width > height ? (float) width / (float) height : (float) height / (float) width;
+        if (width > height) {
+            Matrix.orthoM(mMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, 0f, 10f);
+        } else {
+            Matrix.orthoM(mMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, 0f, 10f);
+        }
     }
 
     @Override
+
     public void onDrawFrame(GL10 gl10) {
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
 
         //准备坐标数据
         GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 0, vertexBuffer);
-        //启用顶点的句柄
+//        //启用顶点的句柄
         GLES30.glEnableVertexAttribArray(0);
+        GLES30.glUniformMatrix4fv(uMatrixLocation, 1, false, mMatrix, 0);
         //绘制三个点
-//        GLES30.glDrawArrays(GLES30.GL_POINTS, 0, 3);
+//        GLES30.glDrawArrays(GLES30.GL_POINTS, 0, 6);
 
         //绘制直线
 //        GLES30.glDrawArrays(GLES30.GL_LINE_STRIP, 0, 2);
 //        GLES30.glLineWidth(10);
 
         //绘制三角形
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_FAN, 0, 6);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_FAN, 0, 362);
 
         //禁止顶点数组的句柄
         GLES30.glDisableVertexAttribArray(0);
+    }
+
+
+    private float[] createPositions() {
+        // 绘制的半径
+        float radius = 0.8f;
+        ArrayList<Float> data = new ArrayList<>();
+        data.add(0.0f);             //设置圆心坐标
+        data.add(0.0f);
+        data.add(0.0f);
+        float angDegSpan = 360f / 360;
+        for (float i = 0; i < 360 + angDegSpan; i += angDegSpan) {
+            data.add((float) (radius * Math.sin(i * Math.PI / 180f)));
+            data.add((float) (radius * Math.cos(i * Math.PI / 180f)));
+            data.add(0.0f);
+        }
+        float[] f = new float[data.size()];
+        for (int i = 0; i < f.length; i++) {
+            f[i] = data.get(i);
+        }
+        return f;
     }
 }
